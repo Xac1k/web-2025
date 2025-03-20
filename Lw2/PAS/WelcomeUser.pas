bb3d8bc9625540ec2243c255cfd2c92b966b6065
Xac1k <145485627+Xac1k@users.noreply.github.com>
2025-03-20 12:33:54 +0300

    Add_Pas_Cgi

--- Lw2/PAS/WelcomeUser.pas
+++ Lw2/PAS/WelcomeUser.pas
@@ -4,19 +4,41 @@ USES
 VAR
   QueryString, User: STRING;
   PosUser: INTEGER;
+
+FUNCTION GetQueryStringParameter(Key: STRING): STRING;
+VAR
+  PosKey, PosMark: INTEGER;
+  QueryString: STRING;
 BEGIN
-  
   QueryString := GetEnv('QUERY_STRING');
-  PosUser := Pos('name=', QueryString) + 5;
-  
-  User := Copy(QueryString, PosUser, Length(QueryString) + 1 - PosUser);
- 
+  PosKey := Pos(Key, QueryString);
+  QueryString := Copy(QueryString, PosKey, Length(QueryString) - PosKey + 1);
+  PosMark := Pos('&', QueryString);
+
+  IF (Copy(GetEnv('QUERY_STRING'), PosKey-1, 1) = '') OR (Copy(GetEnv('QUERY_STRING'), PosKey-1, 1) = '&')
+  THEN
+    BEGIN
+      IF PosMark <> 0
+      THEN
+        QueryString := Copy(QueryString, Length(Key) + 1, PosMark - Length(Key) - 1)
+      ELSE
+        QueryString := Copy(QueryString, Length(Key) + 1, Length(QueryString) - Length(Key));
+       
+      GetQueryStringParameter := QueryString
+    END
+  ELSE
+    GetQueryStringParameter := ''
+END;
+
+BEGIN
   WRITELN('Content-Type: text/plain');
   WRITELN;
+  User := GetQueryStringParameter('name=');
+
   WRITE('Hello dear, ');
   IF User <> ''
   THEN
-    WRITELN(User,'!')
+    WRITELN(User, '!')
   ElSE
     WRITELN('Anonumus!')
 END.
--- Lw2/PAS/WorkWithQueryString.pas
+++ Lw2/PAS/WorkWithQueryString.pas
@@ -9,23 +9,29 @@ VAR
 BEGIN
   QueryString := GetEnv('QUERY_STRING');
   PosKey := Pos(Key, QueryString);
-  QueryString := Copy(QueryString, PosKey, Length(QueryString) - PosKey + 1);
+  QueryString := Copy(QueryString, PosKey, Length(QueryString));
   PosMark := Pos('&', QueryString);
 
-  IF PosMark <> 0
+  IF (Copy(GetEnv('QUERY_STRING'), PosKey-1, 1) = '') OR (Copy(GetEnv('QUERY_STRING'), PosKey-1, 1) = '&')
   THEN
-    QueryString := Copy(QueryString, Length(Key) + 2, PosMark - Length(Key) - 2)
+    BEGIN
+      IF PosMark <> 0
+      THEN
+        QueryString := Copy(QueryString, Length(Key) + 1, PosMark - Length(Key) - 1)
+      ELSE
+        QueryString := Copy(QueryString, Length(Key) + 1, Length(QueryString) - Length(Key));
+       
+      GetQueryStringParameter := QueryString
+    END
   ELSE
-    QueryString := Copy(QueryString, Length(Key) + 2, Length(QueryString) - Length(Key) - 1);
-  
-  GetQueryStringParameter := QueryString 
+    GetQueryStringParameter := ''
 END;
 
 BEGIN {WorkWithQueryString}
   WRITELN('Content-Type: text/plain');
   WRITELN;
-  WRITELN('First Name: ', GetQueryStringParameter('first_name'));
-  WRITELN('Last Name: ', GetQueryStringParameter('last_name'));
-  WRITELN('Age: ', GetQueryStringParameter('age'))
+  WRITELN('First Name: ', GetQueryStringParameter('first_name='));
+  WRITELN('Last Name: ', GetQueryStringParameter('last_name='));
+  WRITELN('Age: ', GetQueryStringParameter('age='))
 END. {WorkWithQueryString}
 
