3b3a344e048f86463070d950b851da06d940e2b6
Xac1k <145485627+Xac1k@users.noreply.github.com>
2025-05-24 00:32:40 +0300

    Add processing more then 1 foto into posts

--- Lw8/home/templeteHome.php
+++ Lw8/home/templeteHome.php
@@ -4,7 +4,15 @@ require_once "../utils.php";
 
 function getHTMLPost(array $data): string
 {
-    $images = getOrDefault($data['urls'][0], 'default_img.png'); 
+
+    $images = '';
+    $pos = 0;
+    foreach ($data['urls'] as $link) 
+    {
+        $pos = $pos + 1;
+        if($pos == 1) $images .= "<img class='image' src='/image/$link' alt='Изображение'>";
+        else $images .= "<img class='image' src='/image/$link' alt='Изображение' hidden>";
+    }
 
     return <<<HTML
                 <div class="frame"> 
@@ -12,7 +20,7 @@ function getHTMLPost(array $data): string
                         <img class="avatar" src="../image/{$data['img_avatar']}" alt="Аватар пользователя">
                         <span class="text name">{$data['name']}</span>
                     </div>
-                    <img class="image" src="/image/{$images}" alt="Изображение">
+                    {$images}
                     <div class="like">
                         <img class = "heart" src="/image/heart.png" alt="Сердечко">
                         <span>{$data['like']}</span> 
